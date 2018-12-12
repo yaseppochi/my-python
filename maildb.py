d5ca64ff4d6ca6a74912157069cd8c868aa35ee6
Stephen J. Turnbull <stephen@xemacs.org>
2018-12-13 00:00:58 +0900

    Complete database building functionality.
    - Implement hash (TODO #4) and length fields of database.
    - Refactor process_mailbox error handling.  Some renaming of locals and
      database fields.
    - Improve reporting in process_mailbox and summarize_database.
    - Use expanduser on file names (TODO #9).  NEEDS AUDIT.
    - Avoid overwriting pickle if no mailboxes are provided.

--- maildb.py
+++ maildb.py
@@ -18,80 +18,128 @@
 # This program takes a set of mbox files or directories full of mbox
 # files, and indexes the messages in them.
 
-import argparse
-import email
-import email.parser
-import email.policy
-import mailbox
-import os
-import os.path
-import pickle
-
 # TO DO
 # 1.  Add a function to list all recipients in a database entry.
-# 2.  Add a function to pretty-print a database entry.
-# 4.  Add a function to hash the payload.  Add a field to the database.
+# 2.  Add a function to pretty-print a database entry.  Use #16.
 # 5.  Add a function to merge database pickles.  Add a CLI argument.
 # 7.  Add a function to identify list posts.
 # 8.  Add a function to prioritize deletions.
 # 9.  Need to expand file names in case not called from shell.
 # 10. Add functionality to migrate database schema.
+# 11. Reorganize imports to separate library functionality from reporting,
+#     testing, and debugging infrastructure.
+# 12. It's hard to see how the database can be reliably updated with additions
+#     to a mailbox because addition to mailbox isn't timestamped.  It makes
+#     sense to use a database other than a dictionary only if it's
+#     significantly faster.  For debug purposes it's worth saving to a pickle,
+#     but maybe the contortions in find_and_load_pickle are unnecessary.
+#     Refactor to emphasize the "one pass" use cases?
+# 13. Need to check for the case of no pickle and no input files?
+# 14. Refactor argument parser to distinguish the default "keep" mailbox, and
+#     present the "k(eep), m(ove to duplicates), d(elete)" dialog efficiently.
+# 15. Refactor database entries to use namedtuple?
+# 16. Add functionality to pretty-print database entries field by field for
+#     easy comparison.  Use these to implement #2.
 
 # Code
 
+import argparse
+from collections import Counter
+import email
+import email.parser
+from email.contentmanager import raw_data_manager
+import email.policy
+from hashlib import sha1
+import mailbox
+import os
+import os.path
+import pickle
+
 def summarize_database(db):
-    print(len(db['missing-message-id@turnbull.sk.tsukuba.ac.jp']),
+    print(len(db.get('missing-message-id@turnbull.sk.tsukuba.ac.jp',())),
           'missing ID')
-    duplicates = 0
-    bodyless = 0
-    for i, (key, values) in enumerate(db.items()):
+    duplicate = hash_mismatch = bodyless = 0
+    ld = Counter((v['bodylength'] for vs in db.values() for v in vs))
+    print(len(ld), 'unique body lengths; min', min(ld.keys()),
+          'max', max(ld.keys()))
+    for key, values in db.items():
         if len(values) > 1:
-            duplicates += 1
+            duplicate += 1
+        hashes = []
         for v in values:
-            bodyless += v['lacks-body']
-    print(duplicates, 'duplicates out of', i, 'unique message IDs')
+            hashes.append(v['bodyhash'])
+            bodyless += 1 if v['bodyhash'] is None else 0
+        for h in hashes:
+            if h != hashes[0]:
+                hash_mismatch += 1
+                #print('Message', key, 'body hashes vary')
+                break
+    print(hash_mismatch, 'hash mismatches and', end=' ')
+    print(duplicate, 'duplicates out of', len(db), 'unique message IDs')
     print(bodyless, 'lacking body')
 
+
+NoneDigest = sha1(b'').digest()
+def get_hash(msg):
+    body = msg.get_body(('related', 'plain', 'html'))
+    # .get_body is idempotent when applied twice with same restriction.
+    body = msg.get_body(('plain', 'html'))
+    content = body.get_content()
+    if isinstance(content, (bytes, str)):
+        content = content.strip()
+        length = len(content)
+        if isinstance(content, str):
+            content = content.encode('utf8', errors='surrogateescape')
+        sha = sha1(content).digest()
+    else:
+        print('content type is', body.get_content_type(),
+              'in', msg['message-id'] or 'message with no id')
+        length = 0
+        sha = NoneDigest
+    return (sha, length)
+
+
 # #### db should be a class and process_mailbox a method
-def process_mailbox(path, db, cls=mailbox.mbox):
+def process_mailbox(path, db, get_mailbox=mailbox.mbox):
     if os.path.isdir(path):
         raise IsADirectoryError(path)
-    errors = [[], []]
-    box = cls(path, create=False)       # #### Probably raises.
-    parser = email.parser.BytesParser(policy=email.policy.default)
-    for k in box.iterkeys():
+    myerrors = {}
+    box = get_mailbox(path, create=False) # #### Probably raises on empty path.
+    for key in box.iterkeys():
         try:
             msgid = 'unparsed-message-id@turnbull.sk.tsukuba.ac.jp'
-            m = parser.parse(box.get_file(k))
-            msgid = m['message-id']
+            parser = email.parser.BytesParser(policy=email.policy.default)
+            msg = parser.parse(box.get_file(key))
+            msgid = msg['message-id']
             if not msgid:         # could be None or '' #### or ' '?            
                 msgid = 'missing-message-id@turnbull.sk.tsukuba.ac.jp'
-            lacks_body = True if m.get_body() is None else False
-        except UnicodeDecodeError:
-            errors[0].append((k, msgid))
-            continue
-        except Exception:
-            errors[1].append((k, msgid))
+            bodyhash, bodylength = get_hash(msg)
+        except Exception as e:
+            name = e.__class__.__name__
+            item = (key, e.args, msgid)
+            if name not in myerrors:
+                myerrors[name] = [item]
+            else:
+                myerrors[name].append(item)
             continue
 
         d = { 'mailbox-path' : path,
-              'unix-from' : m.get_unixfrom(),
-              'lacks-body' : lacks_body }
+              'unix-from' : msg.get_unixfrom(),
+              'bodyhash' : bodyhash,
+              'bodylength' : bodylength }
         for h in ('bcc', 'cc', 'date', 'from', 'message-id', 'to'):
-            d[h] = m.get_all(h)
+            d[h] = msg.get_all(h)
         db[msgid] = value = db.get(msgid, [])
         value.append(d)
 
-    print('skipped due to UnicodeDecodeError:')
-    for msgid in errors[0]:
-        print(' ', msgid)
-    print('skipped due to other Exceptions:')
-    for msgid in errors[1]:
-        print(' ', msgid)
+    for key, errlist in myerrors.items():
+        print('skipped due to ', key, ': ', sep='')
+        for err in errlist:
+            print(' ', err[0], err[1], err[2])
     print('end skips')
 
 def dump_pickle(database_info):
-    outfile = database_info[1] or database_info[2]
+    outfile = database_info[1] or os.path.expanduser(database_info[2])
     if outfile:
         print('Saving pickle to', database_info[2])
         with open(outfile, mode='wb') as f:
@@ -111,7 +159,10 @@ def find_and_load_pickle(path):
 
     db = {}
     # #### This still has a race condition.
-    if path is not None and os.path.exists(path):
+    path = os.path.expanduser(path)
+    if path is None or not os.path.exists(path):
+        path = (None, path)
+    else:
         try:
             with open(path, 'rb') as f:
                 db = pickle.load(f)
@@ -122,8 +173,6 @@ def find_and_load_pickle(path):
             path = tempfile.mkstemp(suffix='-' + os.path.basename(path),
                                     dir=os.path.dirname(path))
             print('will save new database to pickle', path[1])
-    else:
-        path = (None, path)
     return (db,) + path
 
 # tests to add to main program
@@ -204,10 +253,15 @@ if __name__ == '__main__':
     args = parser.parse_args()
 
     db_info = find_and_load_pickle(args.pickle)
+    print('db of length ', len(db_info[0]),
+          ', ', db_info[1],
+          ', ', db_info[2],
+          sep='')
     db = db_info[0]
     for box in args.mailboxes:
         print('processing', box)
         process_mailbox(box, db)
-    dump_pickle(db_info)
+    if args.mailboxes:
+        dump_pickle(db_info)
 
     summarize_database(db)
